fe3589ee35886d75effbb3ae53af18a5c3b79e85
dlrfdnwkd <dlrfdnwkd@naver.com>
2021-07-10 01:31:57 +0900

    mybatis connect test - mybatis

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 practice springboot example
 
 ## version
-spring boot : 2.5.2
-java : 1.8
-tomcat : 9.0.48
-jstl : 1.2
+- spring boot : 2.5.2
+- java : 1.8
+- tomcat : 9.0.48
+- jstl : 1.2
+- mybatis : 2.2.0
--- springbootexample/src/main/java/com/dlrfdnwkd/testspringboot/service/UserService.java
+++ springbootexample/src/main/java/com/dlrfdnwkd/testspringboot/service/UserService.java
@@ -1,17 +1,25 @@
 package com.dlrfdnwkd.testspringboot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dlrfdnwkd.testspringboot.dao.UserDAO;
+import com.dlrfdnwkd.testspringboot.mapper.UserMapper;
 import com.dlrfdnwkd.testspringboot.model.User;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserDAO userDAO;
+	@Autowired
+	private UserMapper mapper;
 	
 	public User findByName(String name) {
 		return userDAO.findByName(name);
+	}
+	public List<User> find(){
+		return mapper.find();
 	}
 }
