ad003852d977f0791f0fc5f562f358da3a8759d6
dlrfdnwkd <dlrfdnwkd@naver.com>
2021-07-09 19:10:33 +0900

    update annotation @RequestMapping -> @GetMapping on TodoController

--- springbootexample/src/main/java/com/dlrfdnwkd/testspringboot/controller/TodoController.java
+++ springbootexample/src/main/java/com/dlrfdnwkd/testspringboot/controller/TodoController.java
@@ -4,8 +4,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,17 +19,17 @@ public class TodoController {
 	private final AtomicInteger counter = new AtomicInteger();
 	
 	//get api - counter 값 확인
-	@RequestMapping(value="/counter", method=RequestMethod.GET)
+	@GetMapping("/counter")
 	public Todo todo(){
 		return new Todo(counter.incrementAndGet(),"테스트중");
 	}
 	//post api - body title 값 확인
-	@RequestMapping(value="/title", method=RequestMethod.POST)
+	@PostMapping("/title")
 	public Todo registryTodo(@RequestParam(value="todoTitle")String todoTitle){
 		return new Todo(counter.incrementAndGet(),todoTitle);
 	}
 	//http header api - 응답 상태값 확인
-	@RequestMapping(value="/status", method=RequestMethod.POST)
+	@PostMapping("/status")
 	public ResponseEntity<Todo> postRegistryTodo(@RequestParam String todoTitle){
 		return new ResponseEntity<>(new Todo(counter.incrementAndGet(),todoTitle),HttpStatus.CREATED);
 	}
